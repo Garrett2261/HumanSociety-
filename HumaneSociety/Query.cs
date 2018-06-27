497df3db086481f7c2e8fef7c65e475aec00967a
Garrett <davisgarrett21@yahoo.com>
2018-06-26 20:11:58 -0500

    Made changes to the Query Class and made all the methods public

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -8,52 +8,52 @@ namespace HumaneSociety
 {
     public static class Query
     {
-        internal static void RunEmployeeQueries(Employee employee, string v)
+        public static void RunEmployeeQueries(Employee employee, string v)
         {
             throw new NotImplementedException();
         }
 
-        internal static Client GetClient(string userName, string password)
+        public static Client GetClient(string userName, string password)
         {
             throw new NotImplementedException();
         }
 
-        internal static object GetUserAdoptionStatus(Client client)
+        public static object GetUserAdoptionStatus(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static object GetAnimalByID(int iD)
+        public static object GetAnimalByID(int iD)
         {
             throw new NotImplementedException();
         }
 
-        internal static void Adopt(object animal, Client client)
+        public static void Adopt(object animal, Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static object RetrieveClients()
+        public static object RetrieveClients()
         {
             throw new NotImplementedException();
         }
 
-        internal static object GetStates()
+        public static object GetStates()
         {
             throw new NotImplementedException();
         }
 
-        internal static void AddNewClient(string firstName, string lastName, string username, string password, string email, string streetAddress, int zipCode, int state)
+        public static void AddNewClient(string firstName, string lastName, string username, string password, string email, string streetAddress, int zipCode, int state)
         {
             throw new NotImplementedException();
         }
 
-        internal static void updateClient(Client client)
+        public static void updateClient(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateUsername(Client client)
+        public static void UpdateUsername(Client client)
         {
             throw new NotImplementedException();
         }
@@ -66,87 +66,87 @@ namespace HumaneSociety
             db.SubmitChanges();
         }
 
-        internal static void UpdateAddress(Client client)
+        public static void UpdateAddress(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateFirstName(Client client)
+        public static void UpdateFirstName(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static object GetPendingAdoptions()
+        public static object GetPendingAdoptions()
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateLastName(Client client)
+        public static void UpdateLastName(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateAdoption(bool v, ClientAnimalJunction clientAnimalJunction)
+        public static void UpdateAdoption(bool v, ClientAnimalJunction clientAnimalJunction)
         {
             throw new NotImplementedException();
         }
 
-        internal static object GetShots(Animal animal)
+        public static object GetShots(Animal animal)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateShot(string v, Animal animal)
+        public static void UpdateShot(string v, Animal animal)
         {
             throw new NotImplementedException();
         }
 
-        internal static void EnterUpdate(Animal animal, Dictionary<int, string> updates)
+        public static void EnterUpdate(Animal animal, Dictionary<int, string> updates)
         {
             throw new NotImplementedException();
         }
 
-        internal static void RemoveAnimal(Animal animal)
+        public static void RemoveAnimal(Animal animal)
         {
             throw new NotImplementedException();
         }
 
-        internal static int? GetBreed()
+        public static int? GetBreed()
         {
             throw new NotImplementedException();
         }
 
-        internal static int? GetLocation()
+        public static int? GetLocation()
         {
             throw new NotImplementedException();
         }
 
-        internal static int? GetDiet()
+        public static int? GetDiet()
         {
             throw new NotImplementedException();
         }
 
-        internal static void AddAnimal(Animal animal)
+        public static void AddAnimal(Animal animal)
         {
             throw new NotImplementedException();
         }
 
-        internal static Employee EmployeeLogin(string userName, string password)
+        public static Employee EmployeeLogin(string userName, string password)
         {
             throw new NotImplementedException();
         }
 
-        internal static Employee RetrieveEmployeeUser(string email, int employeeNumber)
+        public static Employee RetrieveEmployeeUser(string email, int employeeNumber)
         {
             throw new NotImplementedException();
         }
 
-        internal static void AddUsernameAndPassword(Employee employee)
+        public static void AddUsernameAndPassword(Employee employee)
         {
             throw new NotImplementedException();
         }
 
-        internal static bool CheckEmployeeUserNameExist(string username)
+        public static bool CheckEmployeeUserNameExist(string username)
         {
             throw new NotImplementedException();
         }
